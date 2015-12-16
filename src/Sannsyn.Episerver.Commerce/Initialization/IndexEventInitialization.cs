e0a96ad90ebaeadc4addf5127618cf8191075239
evest <steve@celius.cc>
2015-12-16 11:27:28 +0100

    Added project, nuspec and basic Commerce support

---
+++ src/Sannsyn.Episerver.Commerce/Initialization/IndexEventInitialization.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EPiServer;
+using EPiServer.Framework;
+
+namespace Sannsyn.Episerver.Commerce.Initialization
+{
+    [ModuleDependency(typeof(global::EPiServer.Commerce.Initialization.InitializationModule))]
+    public class IndexEventInitialization
+    {
+    }
+}
