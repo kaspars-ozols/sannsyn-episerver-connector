71ec0bcbe8c809049d3d59748d2276f50e7d41f7
sveinung@bvnetwork.no <sveinung@bvnetwork.no>
2015-12-22 15:32:53 +0100

    Store last order id in dds

--- src/Sannsyn.Episerver.Commerce/Initialization/IndexEventInitialization.cs
+++ src/Sannsyn.Episerver.Commerce/Initialization/IndexEventInitialization.cs
@@ -8,6 +8,7 @@ using EPiServer.Framework;
 using EPiServer.Framework.Initialization;
 using EPiServer.ServiceLocation;
 using Mediachase.Commerce.Orders;
+using Sannsyn.Episerver.Commerce.Models;
 using Sannsyn.Episerver.Commerce.Services;
 
 namespace Sannsyn.Episerver.Commerce.Initialization
@@ -20,32 +21,40 @@ namespace Sannsyn.Episerver.Commerce.Initialization
             OrderContext.Current.OrderGroupUpdated += Current_OrderGroupUpdated;
         }
 
-    
+
         private void Current_OrderGroupUpdated(object sender, OrderGroupEventArgs e)
         {
-            //OrderGroupUpdated event are synchronous, should run in a async task
-            Task.Run(() => GenerateSannsynData(sender as OrderGroup, e));
+        
+            GenerateSannsynData(sender as OrderGroup, e);
         }
 
         private void GenerateSannsynData(OrderGroup order, OrderGroupEventArgs e)
         {
-            //if (order.Status.Equals(OrderStatus.InProgress))
             if (order.Status.Equals("InProgress"))
             {
-                ISannsynOrderIndexerService sannsynOrderIndexerService = ServiceLocator.Current.GetInstance<ISannsynOrderIndexerService>();
-                sannsynOrderIndexerService.AddLineItemsToSannsyn(order);
+                SannsynDdsModel lastOrderIndexed = SannsynDdsModel.GetLastOrderIndexed();
+                if (lastOrderIndexed.LastIndexedOrderId < order.OrderGroupId)
+                {
+                    ISannsynOrderIndexerService sannsynOrderIndexerService = ServiceLocator.Current.GetInstance<ISannsynOrderIndexerService>();
+                    sannsynOrderIndexerService.AddLineItemsToSannsyn(order);
+
+                    lastOrderIndexed.LastIndexedOrderId = order.OrderGroupId;
+                    lastOrderIndexed.Save();
+                }
             }
-          
+
+
+
         }
 
         public void Uninitialize(InitializationEngine context)
         {
-            
+
         }
 
         public void ConfigureContainer(ServiceConfigurationContext context)
         {
-            
+
         }
     }
 }
---
+++ src/Sannsyn.Episerver.Commerce/Models/SannsynDDSModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EPiServer.Data;
+using EPiServer.Data.Dynamic;
+
+namespace Sannsyn.Episerver.Commerce.Models
+{
+    public class SannsynDdsModel : IDynamicData
+    {
+
+        public SannsynDdsModel()
+        {
+            Id = Identity.NewIdentity(Guid.NewGuid());
+            
+        }
+
+        // Required to implement IDynamicData
+        public Identity Id { get; set; }
+
+        public int LastIndexedOrderId { get; set; }
+
+        public void Save()
+        {
+            var store = DynamicDataStoreFactory.Instance.CreateStore(typeof(SannsynDdsModel));
+            store.Save(this);
+        }
+
+
+        public static SannsynDdsModel GetLastOrderIndexed()
+        {
+            
+            var store = DynamicDataStoreFactory.Instance.CreateStore(typeof(SannsynDdsModel));
+         
+            var lastOrderIndexed = store.Items<SannsynDdsModel>().FirstOrDefault();
+
+            if (lastOrderIndexed == null)
+                return new SannsynDdsModel();
+            return lastOrderIndexed;
+            
+        }
+    }
+
+}
