8f91ce9b76e811f0b6bafe840125257dc1b8b75e
sveinung@bvnetwork.no <sveinung@bvnetwork.no>
2016-01-04 10:58:35 +0100

    Error handling when sending data to Sannsyn

--- src/Sannsyn.Episerver.Commerce/Initialization/IndexEventInitialization.cs
+++ src/Sannsyn.Episerver.Commerce/Initialization/IndexEventInitialization.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using EPiServer;
 using EPiServer.Framework;
 using EPiServer.Framework.Initialization;
+using EPiServer.Logging;
 using EPiServer.ServiceLocation;
 using Mediachase.Commerce.Orders;
 using Sannsyn.Episerver.Commerce.Models;
@@ -16,6 +17,7 @@ namespace Sannsyn.Episerver.Commerce.Initialization
     [ModuleDependency(typeof(global::EPiServer.Commerce.Initialization.InitializationModule))]
     public class IndexEventInitialization : IConfigurableModule
     {
+        private ILogger _log = LogManager.GetLogger();
         public void Initialize(InitializationEngine context)
         {
             OrderContext.Current.OrderGroupUpdated += Current_OrderGroupUpdated;
@@ -28,19 +30,34 @@ namespace Sannsyn.Episerver.Commerce.Initialization
             GenerateSannsynData(sender as OrderGroup, e);
         }
 
+        /// <summary>
+        /// Adding lineitem entries to Sannsyn. Since OrderGroupUpdated is called often, we add last 
+        /// order indexed in DDS, so we don't index duplicate orders. All code is in a try, then the order 
+        /// will be completed if error occures when calling sannsyn or DDS
+        /// </summary>
+        /// <param name="order"></param>
+        /// <param name="e"></param>
         private void GenerateSannsynData(OrderGroup order, OrderGroupEventArgs e)
         {
             if (order.Status.Equals("InProgress"))
             {
-                SannsynDdsModel lastOrderIndexed = SannsynDdsModel.GetLastOrderIndexed();
-                if (lastOrderIndexed.LastIndexedOrderId < order.OrderGroupId)
+                try
                 {
-                    ISannsynOrderIndexerService sannsynOrderIndexerService = ServiceLocator.Current.GetInstance<ISannsynOrderIndexerService>();
-                    sannsynOrderIndexerService.AddLineItemsToSannsyn(order);
+                    SannsynDdsModel lastOrderIndexed = SannsynDdsModel.GetLastOrderIndexed();
+                    if (lastOrderIndexed.LastIndexedOrderId < order.OrderGroupId)
+                    {
+                        ISannsynOrderIndexerService sannsynOrderIndexerService = ServiceLocator.Current.GetInstance<ISannsynOrderIndexerService>();
+                        sannsynOrderIndexerService.AddLineItemsToSannsyn(order);
 
-                    lastOrderIndexed.LastIndexedOrderId = order.OrderGroupId;
-                    lastOrderIndexed.Save();
+                        lastOrderIndexed.LastIndexedOrderId = order.OrderGroupId;
+                        lastOrderIndexed.Save();
+                    }
                 }
+                catch (Exception ex)
+                {
+                    _log.Error("Could not add order items to Sannsyn", ex);
+                }
+               
             }
 
 
